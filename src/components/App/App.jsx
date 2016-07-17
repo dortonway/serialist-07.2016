0b829789bb3e8326b7ada0116568227910b0cec9
dortonway <dortonway@gmail.com>
2016-07-17 23:36:37 +0300

    converted app component to stateless functional component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,18 @@
 import React, { PropTypes } from 'react';
 import styles from './App.css';
 
-export default class App extends React.Component {
-
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div className={ styles.app }>
-        <div className={ styles.content }>
-          { children }
-        </div>
+const App = ({
+    children
+  }) => (
+    <div className={ styles.app }>
+      <div className={ styles.content }>
+        { children }
       </div>
-    );
-  }
-
-}
+    </div>
+);
 
 App.propTypes = {
   children: PropTypes.object.isRequired
 };
+
+export default App;
